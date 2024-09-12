2159713514c10f7c0309d3cf4f1eb454f8c80ffe
enrell <enrellsa10@proton.me>
2024-09-12 07:55:46 -0300

    Refactor Button component to allow for variable width and customize button style

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
-  final VoidCallback onTap;
+  final Function()? onTap;
   final String text;
+  final double? width;
 
   const Button({
     super.key,
     required this.onTap,
     required this.text,
+    this.width,
   });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: double.infinity,
+      width: width,
       child: ElevatedButton(
         onPressed: onTap,
-        child: Text(text),
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          elevation: 0, // Remove the shadow
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
